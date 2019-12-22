a9f8ddc534059e5f8a6e225e7872dbcc9edfc793
Steve-Kung <stevekung98@gmail.com>
2019-12-22 15:48:13 +0800

    20191222 thymeleaf controller

---
+++ src/main/java/com/steve/springboot/controller/AyUserController.java
@@ -0,0 +1,31 @@
+package com.steve.springboot.controller;
+
+import com.steve.springboot.model.AyUser;
+import com.steve.springboot.service.AyUserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Auther: http://www.stevekung.com
+ * @Date: 2019/12/22
+ * @Description: com.steve.springboot.controller
+ * @version: 1.0
+ */
+@Controller //标注此类是一个控制层类，同时让Spring Boot容器管理起来
+@RequestMapping("/ayUser") // 处理请求地址映射的注解
+public class AyUserController {
+    @Resource
+    private AyUserService ayUserService;
+    @RequestMapping("/test")
+    public String test(Model model){
+        // 查询数据库所有用户
+        // Model接口，可以吧数据库查询出来的数据设置到该类中，前端会从该对象获取数据
+        List<AyUser> ayUser = ayUserService.findAll();
+        model.addAttribute("users", ayUser);
+        return "ayUser";
+    }
+}
